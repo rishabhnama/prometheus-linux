c7973dd8453bd07c6fe8eaf2a317090eaafd08bd
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-05-18 15:44:52 +0530

    Add files via upload

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-git clone https://github.com/rishabhnama/prometheus-linux
+#git clone https://github.com/rishabhnama/prometheus-linux
+
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_8.5.2_amd64.deb
+sudo dpkg -i grafana-enterprise_8.5.2_amd64.deb
+
 wget https://github.com/prometheus/prometheus/releases/download/v2.35.0/prometheus-2.35.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.35.0.linux-amd64.tar.gz
 cp prometheus-linux/prometheus-files/prometheus.yml prometheus-2.35.0.linux-amd64
