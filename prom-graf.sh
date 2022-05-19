a10d30520f7c8fe49fec6e7a48deeeb9bd63f8c1
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-05-18 23:54:36 -0700

    Update prom-graf.sh to start service

--- prom-graf.sh
+++ prom-graf.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y adduser libfontconfig1
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_8.5.2_amd64.deb
 sudo dpkg -i grafana-enterprise_8.5.2_amd64.deb
 
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl start grafana-server
+
 wget https://github.com/prometheus/prometheus/releases/download/v2.35.0/prometheus-2.35.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.35.0.linux-amd64.tar.gz
 cp prometheus-linux/prometheus-files/prometheus.yml prometheus-2.35.0.linux-amd64
