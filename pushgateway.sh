d91bc66e03866a0451511cc717a761583713790c
Rishabh Nama <47190861+rishabhnama@users.noreply.github.com>
2022-05-19 11:43:11 +0530

    Update prom-graf and pushgateway

--- pushgateway.sh
+++ pushgateway.sh
@@ -2,10 +2,11 @@
 
 git clone https://github.com/rishabhnama/prometheus-linux
 
-get https://github.com/prometheus/pushgateway/releases/download/v1.4.2/pushgat>
+wget https://github.com/prometheus/pushgateway/releases/download/v1.4.2/pushgateway-1.4.2.linux-amd64.tar.gz
 tar xvzf pushgateway-1.4.2.linux-amd64.tar.gz
 cd pushgateway-1.4.2.linux-amd64
 ./pushgateway &
+
 
 cd ..
 cd prometheus-linux
